89971f9e7b06b79dc858504f5d85bd34390cbafb
yamazakiemi <emi.yamazaki@person-link.co.jp>
2020-12-25 12:50:51 +0900

    マイグレーション

---
+++ backend/database/migrations/2020_12_24_072011_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id')->comment('投稿ID');
+            $table->unsignedBigInteger('user_id')->comment('ユーザーID');
+            $table->string('title', 255)->comment('投稿タイトル');
+            $table->string('body', 255)->comment('投稿本文');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('作成日時');
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))->comment('更新日時');
+            $table->timestamp('deleted_at')->comment('削除日時');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
